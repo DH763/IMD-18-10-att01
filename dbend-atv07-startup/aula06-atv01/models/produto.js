d928447914d73498a34475df8f735f394cd79074
DH763 <davidh.medeiros2006@gmail.com>
2024-10-26 19:19:13 -0300

    Add files via upload

---
+++ dbend-atv07-startup/aula06-atv01/models/produto.js
@@ -0,0 +1,22 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Produto extends Model {
+    static associate(models) {
+      this.belongsToMany(models.Tag, { through: 'produto_tag', foreignKey: 'produtoId' });
+    }
+  }
+
+  Produto.init({
+    nome: DataTypes.STRING,
+    descricao: DataTypes.STRING,
+    preco: DataTypes.REAL,
+    imagem: DataTypes.STRING 
+  }, {
+    sequelize,
+    modelName: 'Produto',
+  });
+
+  return Produto;
+};
---
+++ dbend-atv07-startup/aula06-atv01/rotas/produtos.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const upload = require('../middleware/upload'); 
+const { Produto } = require('../models');
+
+
+router.post('/:id/upload', upload.single('image'), async (req, res) => {
+  const id = req.params.id;
+  const produto = await Produto.findByPk(id);
+
+  if (!produto) {
+    return res.status(404).json({ msg: "Produto não encontrado!" });
+  }
+
+  if (req.file) {
+    produto.imagem = `/static/uploads/${req.file.filename}`; 
+    await produto.save();
+    return res.json({ msg: "Imagem carregada com sucesso!", imagem: produto.imagem });
+  } else {
+    return res.status(400).json({ msg: "Erro ao fazer upload da imagem." });
+  }
+});
+
+
+router.use('/static', express.static('public/uploads'));
+
+module.exports = router;
